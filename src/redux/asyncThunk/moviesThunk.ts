f499a52c394930491b9dc7ae96c1b52d2545d1e6
ClowKhxu <clowkhuux2707@gmail.com>
2025-02-05 17:31:45 +0700

    cap nhat 1

--- src/redux/asyncThunk/moviesThunk.ts
+++ src/redux/asyncThunk/moviesThunk.ts
@@ -135,7 +135,7 @@ export const getMovieDetail = createAsyncThunk(
   async (rawData: IGetMovieDetail) => {
     let { describe, slug, page, quantity } = rawData;
     try {
-      const baseApi = `https://script.google.com/macros/s/AKfycbxPps30bMsK0LQKVA8LXYO-QOnVaA20C4Tlm3iVTtivvSQFKMao1r4xtaSstizwGUHdIw/exec?path=${describe}/${slug}`;
+      const baseApi = `https://script.google.com/macros/s/AKfycbxPI4DrcMNobScPgdeMtudGsTHlDRDYOJ0wil8WW2Q5cqdV_SkF_LPX06HURltyxtFeow/exec?path=${describe}/${slug}`;
 
       const response = await fetch(`${baseApi}&page=${page}&limit=${quantity}`);
 
